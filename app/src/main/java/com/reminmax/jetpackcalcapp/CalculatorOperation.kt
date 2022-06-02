60c6dfce40e4550e04721c3ce872fa84d1d12229
reminmax <reminmax@gmail.com>
2022-06-02 15:32:14 +0300

    Add classes

---
+++ app/src/main/java/com/reminmax/jetpackcalcapp/CalculatorOperation.kt
@@ -0,0 +1,8 @@
+package com.reminmax.jetpackcalcapp
+
+sealed class CalculatorOperation(val symbol: String) {
+    object Add: CalculatorOperation("+")
+    object Subtract: CalculatorOperation("-")
+    object Multiply: CalculatorOperation("x")
+    object Divide: CalculatorOperation("/")
+}
